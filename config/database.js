9b223ffde6a536df1c3c5baa34dcc5745d56c4ab
Eduardo Prado <eduardoprado@unicauca.edu.co>
2024-05-06 21:30:30 -0500

    feat: added postgres db

--- config/database.js
+++ config/database.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'postgres',
+  logging: false,  // to active log for queries
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+  }
 });
+
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -21,10 +21,14 @@ app.listen(port, () => {
 });
 
 // sync models
-sequelize.sync().then(() => {
-  console.log('Conexión a la base de datos establecida.');
-}).catch(err => {
-  console.error('No se pudo conectar a la base de datos:', err);
-});
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexión establecida exitosamente.');
+        
+        return sequelize.sync();
+    })
+    .catch(err => {
+        console.error('No se pudo conectar a la base de datos:', err);
+    });
 
 app.use('/api', userRoutes);
